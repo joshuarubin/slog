f042a6ea04419fe392e16b3a1a62b8427fa469df
Joshua Rubin <jrubin@zvelo.com>
2016-06-14 15:24:31 -0600

    update json handler

--- handlers/json/json.go
+++ handlers/json/json.go
@@ -3,9 +3,11 @@ package json
 
 import (
 	j "encoding/json"
+	"fmt"
 	"io"
 	"os"
 	"sync"
+	"time"
 
 	"jrubin.io/slog"
 )
@@ -32,9 +34,38 @@ func New(w io.Writer) *Handler {
 	}
 }
 
+type entry struct {
+	Fields  slog.Fields `json:"fields"`
+	Level   slog.Level  `json:"level"`
+	Time    time.Time   `json:"time"`
+	Message string      `json:"msg"`
+}
+
+func newEntry(e *slog.Entry) *entry {
+	ret := &entry{
+		Fields:  slog.Fields{},
+		Level:   e.Level,
+		Time:    e.Time,
+		Message: e.Message,
+	}
+
+	for key, value := range e.Fields {
+		switch value := value.(type) {
+		case fmt.Stringer:
+			ret.Fields[key] = value.String()
+		case error:
+			ret.Fields[key] = value.Error()
+		default:
+			ret.Fields[key] = value
+		}
+	}
+
+	return ret
+}
+
 // HandleLog implements slog.Handler.
 func (h *Handler) HandleLog(e *slog.Entry) error {
 	h.mu.Lock()
 	defer h.mu.Unlock()
-	return h.enc.Encode(e)
+	return h.enc.Encode(newEntry(e))
 }
